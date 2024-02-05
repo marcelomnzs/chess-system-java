f5c09431e1aae8862528eb127546e77e4cb479f2
Marcelo Antonio <marceloa.mnzs@gmail.com>
2024-02-05 10:55:21 -0300

    Implementing check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -54,6 +54,12 @@ public class UI {
         System.out.println();
         System.out.println("Turn: " + chessMatch.getTurn());
         System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+
+        if(chessMatch.getCheck()) {
+            System.out.print(ANSI_RED_BACKGROUND);
+            System.out.println("CHECK!");
+            System.out.print(ANSI_RESET);
+        }
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
